17f2084c032fa9ebad835b4c182328bd7de7c88c
Pyrogrammer404 <105351111+Pyrogrammer404@users.noreply.github.com>
2022-10-01 00:49:44 +0530

    Update amazon_test_Q1.py
    Increased the speed and efficiency of the code by changing several functions

--- amazon_test_Q1.py
+++ amazon_test_Q1.py
@@ -7,4 +7,21 @@ for i in range(len(locations)):
             locations[i] = movedto[j]
 locations.sort()
 print(locations)
-
+locations = [1,5,2,6]
+movedf = [1,4,5,7]
+movedto = [4,7,1,3]
+for i in range(len(locations)):
+    for j in range(len(movedf)):
+        if movedf[j] == locations[i]:
+            locations[i] = movedto[j]
+locations.sort()
+print(locations)
+locations = [1,5,2,6]
+movedf = [1,4,5,7]
+movedto = [4,7,1,3]
+for i in range(len(locations)):
+    for j in range(len(movedf)):
+        if movedf[j] == locations[i]:
+            locations[i] = movedto[j]
+locations.sort()
+print(locations)
