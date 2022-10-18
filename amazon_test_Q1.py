c348dcfd1fa59422aefd7bcdb57b66ab364df814
AbhayG-sudo <66205656+AbhayG-sudo@users.noreply.github.com>
2022-10-18 15:42:33 +0530

    Merge Sort
    improved sorting algorithm to pass more test cases which were failing due to TLE(Time Limit Exceeded)

--- amazon_test_Q1.py
+++ amazon_test_Q1.py
@@ -1,11 +1,19 @@
 locations = [1,5,2,6]
 movedf = [1,4,5,7]
 movedto = [4,7,1,3]
+#merge sort
+MergeSort(A, p, r):
+    if p > r 
+        return
+    q = (p+r)/2
+    mergeSort(A, p, q)
+    mergeSort(A, q+1, r)
+    merge(A, p, q, r)
 for i in range(len(locations)):
     for j in range(len(movedf)):
         if movedf[j] == locations[i]:
             locations[i] = movedto[j]
-locations.sort()
+locations.MergeSort()
 print(locations)
 locations = [1,5,2,6]
 movedf = [1,4,5,7]
@@ -14,7 +22,7 @@ for i in range(len(locations)):
     for j in range(len(movedf)):
         if movedf[j] == locations[i]:
             locations[i] = movedto[j]
-locations.sort()
+locations.MergeSort()
 print(locations)
 locations = [1,5,2,6]
 movedf = [1,4,5,7]
@@ -23,5 +31,5 @@ for i in range(len(locations)):
     for j in range(len(movedf)):
         if movedf[j] == locations[i]:
             locations[i] = movedto[j]
-locations.sort()
+locations.MergeSort()
 print(locations)
